0c3432049bd651ffc0bfaf2ba81047f447712da2
Shafayat <mindfreakicup33@gmail.com>
2019-12-09 02:20:06 +0600

    Toolbar menu and Logout

--- app/src/main/java/com/baldystudios/daggerkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/baldystudios/daggerkotlin/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.baldystudios.daggerkotlin.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.baldystudios.daggerkotlin.BaseActivity
 import com.baldystudios.daggerkotlin.R
 
@@ -10,4 +12,26 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+
+            R.id.menu_logout ->{
+                sessionManager.logout()
+                return true
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
