696da296256e64f2bd008c6724efc75fd44ee121
shafayat <mamunshafayatbin@gmail.com>
2019-12-11 15:21:20 +0600

    Dagger 2, fragments and view models

--- app/src/main/java/com/baldystudios/daggerkotlin/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/baldystudios/daggerkotlin/ui/main/profile/ProfileFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.baldystudios.daggerkotlin.R
+import com.baldystudios.daggerkotlin.models.User
+import com.baldystudios.daggerkotlin.ui.auth.AuthResource
 import com.baldystudios.daggerkotlin.viewmodels.ViewModelProvidersFactory
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 import javax.inject.Inject
 
 class ProfileFragment : DaggerFragment() {
@@ -40,6 +44,53 @@ class ProfileFragment : DaggerFragment() {
             .get(ProfileViewModel::class.java)
 
         profileViewModel.test()
+
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+
+        profileViewModel.getAuthenticatedUser().removeObservers(viewLifecycleOwner)
+        profileViewModel.getAuthenticatedUser()
+            .observe(viewLifecycleOwner, Observer<AuthResource<User>> {
+
+                it?.let { userAuthResource ->
+
+                    when (userAuthResource::class) {
+
+                        AuthResource.Authenticated::class -> {
+                            setUserDetails(userAuthResource.data)
+                            Log.d(TAG, "OnChanged: ${userAuthResource.data?.email}")
+                        }
+
+                        AuthResource.Error::class -> {
+                            setErrorDetails(userAuthResource.message)
+                            Log.e(TAG, "OnChanged: ${userAuthResource.message}")
+                        }
+
+                        else -> {
+                            Log.e(TAG, "OnChanged: Nothing is working")
+                        }
+                    }
+
+                }
+
+            })
+    }
+
+    private fun setErrorDetails(message: String) {
+
+        email.text = message
+        username.text = "error"
+        website.text = "error"
+    }
+
+    private fun setUserDetails(user: User?) {
+
+        email.text = user?.email
+        username.text = user?.name
+        website.text = user?.website
+
     }
 
 }
--- app/src/main/java/com/baldystudios/daggerkotlin/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/baldystudios/daggerkotlin/ui/main/profile/ProfileViewModel.kt
@@ -1,15 +1,24 @@
 package com.baldystudios.daggerkotlin.ui.main.profile
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.baldystudios.daggerkotlin.SessionManager
+import com.baldystudios.daggerkotlin.models.User
+import com.baldystudios.daggerkotlin.ui.auth.AuthResource
 import javax.inject.Inject
 
-class ProfileViewModel @Inject constructor(): ViewModel() {
+class ProfileViewModel @Inject constructor(val sessionManager: SessionManager) : ViewModel() {
 
     val TAG = "ProfileViewModel"
 
-    fun test(){
+    fun test() {
         Log.v(TAG, "Profile View Model was created")
+    }
+
+
+    fun getAuthenticatedUser(): LiveData<AuthResource<User>> {
+        return sessionManager.authUser
     }
 
 }
